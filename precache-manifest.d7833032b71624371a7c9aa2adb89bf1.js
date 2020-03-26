2ea95206f5adc4744bd07486d39e45a1181b644d
Iamhamzaarif <hamza.arif5587@gmail.com>
2020-03-27 02:28:04 +0500

    Updates

---
+++ precache-manifest.d7833032b71624371a7c9aa2adb89bf1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fda9ba3e737ee5e0adafe7fdac3d7c3c",
+    "url": "/monster-roledex/index.html"
+  },
+  {
+    "revision": "2e26d772a2ca28626d49",
+    "url": "/monster-roledex/static/css/main.06412fe8.chunk.css"
+  },
+  {
+    "revision": "1bb5dd2a6657ff2ee685",
+    "url": "/monster-roledex/static/js/2.b6f2604e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-roledex/static/js/2.b6f2604e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e26d772a2ca28626d49",
+    "url": "/monster-roledex/static/js/main.4722474e.chunk.js"
+  },
+  {
+    "revision": "1d95995a5d70377be3f5",
+    "url": "/monster-roledex/static/js/runtime-main.c1e49c61.js"
+  }
+]);
